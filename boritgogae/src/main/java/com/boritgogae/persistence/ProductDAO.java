c772da9bd1573eca61774f0a6fc0e73459caa936
frogwing <dnjaos12345@gmail.com>
2022-10-26 19:16:29 +0900

    메인페이지,상품리스트페이지 유효성,기능끝

--- boritgogae/src/main/java/com/boritgogae/persistence/ProductDAO.java
+++ boritgogae/src/main/java/com/boritgogae/persistence/ProductDAO.java
@@ -1,6 +1,7 @@
 package com.boritgogae.persistence;
 
 import java.util.List;
+import java.util.Map;
 
 import com.boritgogae.board.tip.domain.TipPagingInfo;
 import com.boritgogae.domain.ProductDTO;
@@ -11,7 +12,13 @@ public interface ProductDAO {
 
 	public List<ProductDTO> LastProduct() throws Exception;
 	
-	public List<ProductDTO> getProdInfo(TipPagingInfo pi) throws Exception;
+	public int getProdCnt(int pageNo, String category) throws Exception;
 
-	public int getProdCnt() throws Exception;
+	public List<ProductDTO> getProductAll(String category, TipPagingInfo pi) throws Exception;
+
+	public int getSearchProdCnt(int pageNo, String category) throws Exception;
+
+	public List<ProductDTO> getSearchProductAll(String category, TipPagingInfo pi) throws Exception;
+
+
 }
--- boritgogae/src/main/java/com/boritgogae/persistence/ProductDAOImpl.java
+++ boritgogae/src/main/java/com/boritgogae/persistence/ProductDAOImpl.java
@@ -1,6 +1,8 @@
 package com.boritgogae.persistence;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.inject.Inject;
 
@@ -30,19 +32,59 @@ public class ProductDAOImpl implements ProductDAO {
 
 	@Override
 	public List<ProductDTO> LastProduct() throws Exception {
-
+		
 		return ses.selectList(ns+".LastProduct");
 	}
 	
-	@Override
-	public List<ProductDTO> getProdInfo(TipPagingInfo pi) {
-		
-		return ses.selectList(ns+".prodList",pi);
-	}
 
 	@Override
-	public int getProdCnt() throws Exception {
-		return ses.selectOne(ns+".prodCnt");
+	public int getProdCnt(int pageNo, String category) throws Exception {
+		Map<String, String> map = new HashMap<>();
+		map.put("category", category);
+		map.put("pageNo", pageNo+"");
+		int s = ses.selectOne(ns+".prodCnt",map);
+		return ses.selectOne(ns+".prodCnt",map);
 	}
+
+
+	@Override
+	public List<ProductDTO> getProductAll(String category, TipPagingInfo pi) throws Exception {
+		Map<String, Object> map = new HashMap<>();
+		String startNum = pi.getStartNum()+"";
+		String PostPerPage = pi.getPostPerPage()+"";
+		map.put("category", category);
+		map.put("StartNum", pi.getStartNum());
+		map.put("PostPerPage", pi.getPostPerPage());
+		
+		return ses.selectList(ns+".ProductAll", map);
+	}
+
+
+	@Override
+	public int getSearchProdCnt(int pageNo, String category) throws Exception {
+		
+		System.out.println(ses.selectOne(ns+".productSearchCnt", category)+"@@@검색 결과상품갯수");
+		
+		return ses.selectOne(ns+".productSearchCnt", category);
+	}
+
+
+	@Override
+	public List<ProductDTO> getSearchProductAll(String category, TipPagingInfo pi) throws Exception {
+		Map<String, Object> map = new HashMap<>();
+		map.put("category", category);
+		map.put("StartNum", pi.getStartNum());
+		map.put("PostPerPage", pi.getPostPerPage());
+		
+		System.out.println(pi.getStartNum()+"이게 문제인가");
+		System.out.println(category+"검색키워드는 잘들어오는가");
+		System.out.println(pi.getPostPerPage()+"끝 페이지 이거는 들어오는가");
+		
+		return ses.selectList(ns+".productSearchList", map);
+	}
+
+
+
+
 
 }
